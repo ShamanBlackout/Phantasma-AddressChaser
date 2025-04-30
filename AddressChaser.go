284ece937be03f349933446828c73d486935b2b8
ShamanBlackout <47762410+ShamanBlackout@users.noreply.github.com>
2025-04-30 18:36:39 -0500

    Address Chaser Changes

--- AddressChaser.go
+++ AddressChaser.go
@@ -3,38 +3,33 @@ package main
 import (
 	"fmt"
 
+	"github.com/phantasma-io/phantasma-go-admin-tools/pkg/analysis"
 	"github.com/phantasma-io/phantasma-go/pkg/rpc"
-	"github.com/phantasma-io/phantasma-go/pkg/rpc/response"
 )
 
-var netSelected string
+//var clients []rpc.PhantasmaRPC
 var client rpc.PhantasmaRPC
-var chainTokens []response.TokenResult
-
-// AddressChaser is a struct that holds the address and the RPC client
-type AddressChaser struct {
-	Address string
-	client  rpc.PhantasmaRPC
-}
-
-func printTokens() {
-	for _, t := range chainTokens {
-		fmt.Println(t.Symbol, "flags:", t.Flags)
-	}
-}
 
 
 func main() {
+	
+	
+		//clients = rpc.NewRPCSetMainnet()
+		//clients := clients[:2]
+		
+		clients := []rpc.PhantasmaRPC{
+			rpc.NewRPC("https://pharpc1.phantasma.info/rpc"),
+			rpc.NewRPC("https://pharpc2.phantasma.info/rpc"),
+		}
+		//save block height
+	
+		addresses := analysis.GetAllKnownAddresses(clients,"", true);
+	
+	
+	
+		for _, r := range addresses {
+			fmt.Printf("%s,", r)
+		}
 
-	client = rpc.NewRPCMainnet()
-	chainTokens, _ = client.GetTokens(false)
-	fmt.Println("Received information about", len(chainTokens), netSelected, "tokens")
 
-	printTokens()
-	// t := getChainToken("SOUL")
-	// fmt.Println(t.Symbol, "fungible:", t.IsFungible(), "fuel:", t.IsFuel(), "stakable:", t.IsStakable(), "burnable:", t.IsBurnable(), "transferable:", t.IsTransferable())
-	// t = getChainToken("CROWN")
-	// fmt.Println(t.Symbol, "fungible:", t.IsFungible(), "fuel:", t.IsFuel(), "stakable:", t.IsStakable(), "burnable:", t.IsBurnable(), "transferable:", t.IsTransferable())
-	// t = getChainToken("KCAL")
-	// fmt.Println(t.Symbol, "fungible:", t.IsFungible(), "fuel:", t.IsFuel(), "stakable:", t.IsStakable(), "burnable:", t.IsBurnable(), "transferable:", t.IsTransferable())
-}
+	}
--- go.mod
+++ go.mod
@@ -2,6 +2,18 @@ module github.com/ShamanBlackout/Phantasma/Go-Tasma
 
 go 1.24.2
 
-require github.com/phantasma-io/phantasma-go v0.0.0-20250416032013-5cd71c2a2228
+require (
+	github.com/jessevdk/go-flags v1.6.1
+	github.com/phantasma-io/phantasma-go v0.0.0-20250226232435-0a00b9b0de7e
+	github.com/phantasma-io/phantasma-go-admin-tools v0.0.0-20250306232410-5e89da0108aa
+)
 
-require github.com/mr-tron/base58 v1.2.0 // indirect
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
