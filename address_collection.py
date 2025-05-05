4b7a91a77ee01e2f555818febb9244827ad86ae5
ShamanBlackout <47762410+ShamanBlackout@users.noreply.github.com>
2025-05-04 21:59:33 -0500

    Soul Balance support
    Adding support to calculate all soul balances in a sorted order from most to least. Similar to a leaderboard

--- address_collection.py
+++ address_collection.py
@@ -1,27 +1,31 @@
 import requests
 import json
+"""
+Created by: Shaman Blackout
+Created on 05-0405-2025 
+This script fetches address data from the Phantasma API and saves it to a JSON file.
+It also provides functions to count the number of stake masters and organize soul balances.
+"""
+LIMIT = "1000000" #max values to be pulled
+OFFSET = "0"
+CHAIN = "main"
+ORDER_BY = ("id","address","address_name")
+ORDER_DIRECTION = ("asc","desc")
+VALIDATOR_KIND =["Invalid","Primary","Secondary","Proposed","Invalid"]
+SHOW_ALL = ('0','1')#used for with_storage, with_stakes, with_balance, with_total , 0 for false, 1 for true
 
-limit = "1000000" #max values to be pulled
-offset = "0" #
-chain = "main"
-order_by = "id" #id, address, address_name 
-order_direction = "asc"
-validator_kind =["Invalid","Primary","Secondary","Proposed","Invalid"]
-with_storage = "1"  # 0 default    
-with_stakes = "1" # 0 default
-with_balance = "1" # 0 default
-with_total = "1" # 0 or 1, 0 default 
 
 
 
-url = "https://api-explorer.phantasma.info/api/v1/addresses?order_by="+order_by +\
-"&order_direction="+order_direction+"&offset="+offset+"&limit="+limit+"&chain="+chain
+url = "https://api-explorer.phantasma.info/api/v1/addresses?ORDER_BY="+ORDER_BY[0] +\
+"&ORDER_DIRECTION="+ORDER_DIRECTION[0]+"&OFFSET="+OFFSET+"&LIMIT="+LIMIT+"&CHAIN="+CHAIN+\
+"&with_storage="+SHOW_ALL[0]+"&with_stakes="+SHOW_ALL[1]+"&with_balance="+SHOW_ALL[1]+"&with_total="+SHOW_ALL[1]
 
 
 def mastersCount(filename):
     
     """
-    Counts the number of stake masters on chain.
+    Counts the number of stake masters on CHAIN.
 
     Args:
         filename (str): The name of the file to count lines in.
@@ -48,18 +52,44 @@ def mastersCount(filename):
         "stake_masters": stake_masters  
     }
     if stake_masters:
-        with open("stakeMasters"".json", "w") as f:
+        with open("stakeMasters.json", "w") as f:
             json.dump(stake_masters_count, f, indent=4)  # Save with indentation for readability)
-        
-        
+    print("Successfully fetched and saved data to stakeMasters.json")
 
 
+def SoulBalances(filename):
+    """
+    Organizes the soul balances of addresses in a JSON file and saves them to a new file.
 
-  
-    
+    Args:
+        filename (str): The name of the file to count lines in.
+    """
+    soul_balances = [];
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            for item in data["addresses"]:
+                amount = None
+                for balance in item['balances']:
+                    if balance["token"]["symbol"] == "SOUL" and balance["chain"]["chain_name"] == "main":
+                        amount = float(balance["amount"]) +float(item["stake"])
+                if amount: ## Check if amount is not None or empty. Either SOUL is staked or they have no SOUL balance.
+                        soul_balances.append({
+                            "address":item['address'],
+                            "balance":amount})
+                else: 
+                    soul_balances.append({
+                        "address":item['address'],
+                        "balance":float(item['stake'])})      
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file {filename}.")
 
-    
-
+    if soul_balances:
+        with open("soulBalances.json", "w") as f:
+            json.dump(sorted(soul_balances, key=lambda x: x["balance"])[::-1], f, indent=4)  # Save with indentation for readability)
+    print("Successfully fetched and saved data to soulBalances.json")
 
 def fetch_and_save_address_collection(url,validator):
     """
@@ -69,7 +99,7 @@ def fetch_and_save_address_collection(url,validator):
         url (str): The URL to fetch the JSON data from.
     """
     try:
-        url = url + "&validator_kind=" + validator
+        url = url + "&VALIDATOR_KIND=" + validator
         response = requests.get(url)
         response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
 
@@ -87,9 +117,14 @@ def fetch_and_save_address_collection(url,validator):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+def update_all():
+    #Will be used to update all the address collections in the future.
+    print("Updating all address collections...")
 
 
 if __name__ == "__main__":
-    #for validator in validator_kind:
-    #   fetch_and_save_address_collection(url,validator)
+    for validator in VALIDATOR_KIND:
+        fetch_and_save_address_collection(url,validator)
     mastersCount("addressCollection_Invalid.json")
+    SoulBalances("addressCollection_Invalid.json")
+   
